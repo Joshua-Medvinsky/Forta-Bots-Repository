c2adcb4c799c3a916ce9a7f6fa2cb79ed0edd40f
Joshua-Medvinsky <joshuamedvinsky33@gmail.com>
2023-11-25 21:24:53 -0800

    Fix: put challenges in seperate folders in the same repo

---
+++ Forta-Bots-Challenge-1/README.md
@@ -0,0 +1,27 @@
+# Forta-Bots-Challenges
+
+## Description
+
+This agent detects when a bot is deployed from Nethermind Deployer Address (Nethermind Forta deployer (address: 0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8)
+
+## Supported Chains
+
+- Ethereum
+
+
+## Alerts
+
+Describe each of the type of alerts fired by this agent
+
+- FORTA-1
+  - Fired when a Nethermind Forta deployer address deploys a bot: 0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8
+  - Severity is always set to "low" (mention any conditions where it could be something else)
+  - Type is always set to "info" (mention any conditions where it could be something else)
+  - Mention any other type of metadata fields included with this alert
+
+## Test Data
+
+The agent behaviour can be verified with the following transactions:
+
+- (x transaction address, to be decided on)
+
---
+++ Forta-Bots-Challenge-1/src/agent.ts
@@ -0,0 +1,46 @@
+import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
+import { FORTA_BOT_REGISTRY, BOT_DEPLOYER_ADDRESS, NEW_AGENT_FUNCTION_SIGNATURE } from "./constants";
+
+export function provideHandleTransaction(
+  fortaRegistryAddress: string,
+  nethermindDeployerAddress: string,
+  functionABI: string
+): HandleTransaction {
+  return async function handleTransaction(txEvent: TransactionEvent) {
+    const findings: Finding[] = [];
+    //check from address with deployer address
+    if (txEvent.from != nethermindDeployerAddress.toLowerCase()) {
+      return findings;
+    }
+
+    // Store/filter bot transactions
+    const newAgentTxs = txEvent.filterFunction(functionABI, fortaRegistryAddress);
+
+    // Iterate through transactions
+    newAgentTxs.forEach((tx) => {
+      const { agentId, owner, chainIds, metadata } = tx.args;
+      //Create a Finding object and push it into the findings array
+      findings.push(
+        Finding.fromObject({
+          name: "Nethermind Bot Deployment Detector",
+          description: "Detects Bots Deployed by Nethermind",
+          alertId: "FORTA-123",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          metadata: {
+            agentId: agentId.toString(),
+            owner,
+            chainIds: chainIds.toString(),
+            metadata,
+          },
+        })
+      );
+    });
+
+    return findings;
+  };
+}
+
+export default {
+  handleTransaction: provideHandleTransaction(FORTA_BOT_REGISTRY, BOT_DEPLOYER_ADDRESS, NEW_AGENT_FUNCTION_SIGNATURE),
+};
