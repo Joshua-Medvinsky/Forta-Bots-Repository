2f473d10717821476b647b6f785bfc96518f11bb
Joshua-Medvinsky <joshuamedvinsky33@gmail.com>
2024-02-19 14:34:04 -0800

    Fix: file name change

--- Forta-Bot-Challenge-4/COMP01/src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const SUPPLY_EVENT_SIGNATURE: string = "event Supply(address indexed from, address indexed dst, uint amount)";
-
-export const WRONG_SUPPLY_EVENT_SIGNATURE: string =
-  "event fooSupply(address indexed from, address indexed dst, uint amount)";
-
-export const PROXY_CONTRACT_ADDRESS: string = "0xc3d688B66703497DAA19211EEdff47f25384cdc3";
-
-export const SUPPLY_POOL_ADDRESS: string = "0xc3d688B66703497DAA19211EEdff47f25384cdc3";
